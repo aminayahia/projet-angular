276c24e55355144d3f57b678d84a012f0c7510e9
aminayahia <amina.y1990@gmail.com>
2024-06-29 18:31:18 +0100

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',redirectTo:'login',pathMatch:'full'
+    },
+    {
+        path:'login', component:LoginComponent
+    },
+    {
+        //show LayoutComponent and DashbordComponent after login
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'dashbord',
+                component:DashbordComponent
+            }
+
+        ]
+    }
+];
