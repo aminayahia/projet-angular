ac7f8f9b6fbf3e537b263c2be02edc53723f168d
aminayahia <amina.y1990@gmail.com>
2024-06-29 21:26:14 +0100

    add all componnets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
+import { SallesComponent } from './salles/salles.component';
+import { NewReservationComponent } from './new-reservation/new-reservation.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
 export const routes: Routes = [
     {
@@ -18,7 +21,19 @@ export const routes: Routes = [
             {
                 path:'dashbord',
                 component:DashbordComponent
-            }
+            },
+            {
+                path:'salles',
+                component:SallesComponent
+            },
+            {
+                path:'newReservations',
+                component:NewReservationComponent
+            },
+            {
+                path:'reservations',
+                component:ReservationsComponent
+            },
 
         ]
     }
---
+++ src/app/salles/salles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salles',
+  standalone: true,
+  imports: [],
+  templateUrl: './salles.component.html',
+  styleUrl: './salles.component.css'
+})
+export class SallesComponent {
+
+}
